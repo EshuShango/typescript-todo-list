cdbbc7f3f4e353039b62cb7d0374df7d9457dda1
EshuShango <tobiakinyemi21@gmail.com>
2023-04-13 16:52:30 -0500

    first commit

--- build/dist/index.js
+++ build/dist/index.js
@@ -1,50 +1,32 @@
-import { v4 as uuidV4 } from "uuid";
-
-type Task = {
-  id: string;
-  title: string;
-  completed: boolean;
-  createdAt: Date;
-};
-
+import {v4 as uuidV4} from "../_snowpack/pkg/uuid.js";
 console.log(uuidV4());
-
-const clearTasksButton = document.getElementById(
-  "clear-tasks",
-) as HTMLButtonElement | null;
-
-const list = document.querySelector<HTMLUListElement>("#list");
-// const form = document.querySelector<HTMLFormElement>('#new-task-form');
-const form = document.getElementById("new-task-form") as HTMLFormElement | null;
-
-const input = document.querySelector<HTMLInputElement>("#new-task-item");
-
-const tasks: Task[] = loadTasks();
+const clearTasksButton = document.getElementById("clear-tasks");
+const list = document.querySelector("#list");
+const form = document.getElementById("new-task-form");
+const input = document.querySelector("#new-task-item");
+const tasks = loadTasks();
 tasks.forEach(addListItem);
-
 form?.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (input?.value == "" || input.value == null) return;
-  const newTask: Task = {
+  if (input?.value == "" || input.value == null)
+    return;
+  const newTask = {
     id: uuidV4(),
     title: input.value,
     completed: false,
-    createdAt: new Date(),
+    createdAt: new Date()
   };
   tasks.push(newTask);
   saveTasks();
-
   tasks.pop(newTask);
   saveTasks();
-
   addListItem(newTask);
   input.value = "";
 });
-
 clearTasksButton?.addEventListener("click", () => {
   clearTasks();
 });
-function addListItem(task: Task) {
+function addListItem(task) {
   const item = document.createElement("li");
   const label = document.createElement("label");
   const checkbox = document.createElement("input");
@@ -58,27 +40,21 @@ function addListItem(task: Task) {
   item.append(label);
   list?.append(item);
 }
-
 function saveTasks() {
   localStorage.setItem("TASKS", JSON.stringify(tasks));
 }
-
-function loadTasks(): Task[] {
+function loadTasks() {
   const taskJSON = localStorage.getItem("TASKS");
-  if (taskJSON == null) return [];
+  if (taskJSON == null)
+    return [];
   return JSON.parse(taskJSON);
 }
-
-function clearTasks(): void {
-  tasks.length = 0; // Empty the tasks array
-  localStorage.removeItem("TASKS"); // Remove tasks from local storage
-
-  // Remove all child elements from the list
+function clearTasks() {
+  tasks.length = 0;
+  localStorage.removeItem("TASKS");
   if (list) {
     while (list.firstChild) {
       list.removeChild(list.firstChild);
     }
   }
 }
-
-//^ the ? is optional chaining
